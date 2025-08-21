67775104f347900c0ecf71efccdf1ef1b935a184
unknown <robertooliveira0751@digitalcollege.internal>
2025-08-21 20:41:19 -0300

    Primeiro Git

---
+++ Script.js/Script.js
@@ -0,0 +1,19 @@
+
+
+
+var lista = [
+    { nome: "alex" },
+    { nome: "Roberto" },
+    { nome: "Daniel" },
+    { nome: "julio" },
+    { nome: "João Paulo"},
+];
+
+for (var indice = 2; indice < lista.length; indice++){
+    if (lista[indice].nome === "julio") {
+        console.log(lista[indice].nome);
+
+    }
+
+}
+
